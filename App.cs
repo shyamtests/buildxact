727d00f2d57fd4022f47ebd5778fd068c7a4f951
Shyam Saraswati <shyam.saraswati@gmail.com>
2020-05-13 10:18:51 +1000

    Setup configuration and Dependency Injection

---
+++ App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace SuppliesPriceLister
+{
+    public class App
+    {
+        private readonly IConfiguration _config;
+
+        public App(IConfiguration config)
+        {
+            _config = config;
+        }
+        
+        public void Run()
+        {
+            Console.WriteLine($"Hello from App.cs exchange rate is {_config.GetValue<decimal>("audUsdExchangeRate")}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SuppliesPriceLister
 {
@@ -6,7 +9,35 @@ namespace SuppliesPriceLister
     {
         static void Main(string[] args)
         {
-            // Your solution begins here
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetService<App>().Run();
         }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+
+
+            services.AddTransient<App>();
+
+            return services;
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+
     }
 }
